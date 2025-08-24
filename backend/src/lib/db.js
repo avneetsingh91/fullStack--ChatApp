78fc7aa989eab436582f9ada47b60075c6be23b4
Avneet Singh <avneetsingh7860@gmail.com>
2025-08-25 01:48:54 +0530

    project completed

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import mongoose, { mongo } from "mongoose"
+
+export const connectDB = async()=>{
+
+    try {
+        
+   
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
+    console.log(`Mongodb Connected: ${conn.connection.host} `)
+
+     } catch (error) {
+        console.log("MongoDb connection error ", error)
+    }
+}
